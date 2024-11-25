d9e1e0dab814dc521ad1f5d03cad9ab38891e89b
Vladislav <81586390+SumetsVladislav@users.noreply.github.com>
2024-11-25 14:44:05 +0200

    Реалізовано Constructor Injection у класі User

--- user.py
+++ user.py
@@ -1,10 +1,11 @@
 from interfaces import IUser
 
 class User(IUser):
-    def __init__(self, user_id, username, email):
+    def __init__(self, user_id, username, email, dependency=None):
         self.user_id = user_id
         self.username = username
         self.email = email
+        self.dependency = dependency  # Нове поле для впровадження залежностей
 
     def get_user_info(self):
         return {
@@ -12,4 +13,10 @@ class User(IUser):
             "username": self.username,
             "email": self.email,
         }
+
+    def do_something(self):
+        if self.dependency:
+            return self.dependency.action()
+        else:
+            return "No dependency provided"
 
